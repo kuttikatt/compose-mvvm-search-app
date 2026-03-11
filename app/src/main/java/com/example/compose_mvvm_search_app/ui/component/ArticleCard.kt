40063be0fe7149cfc83f47f8f638a95e3a166090
kuttikatt1995 <reshmakuttikatt@gmail.com>
2026-03-11 13:01:36 +0530

    refactor UI components and improve navigation implementation

--- app/src/main/java/com/example/compose_mvvm_search_app/ui/component/ArticleCard.kt
+++ app/src/main/java/com/example/compose_mvvm_search_app/ui/component/ArticleCard.kt
@@ -1,4 +1,54 @@
 package com.example.compose_mvvm_search_app.ui.component
 
-class ArticleCard {
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun AddCardData(article: Int, navController: NavHostController) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable {
+                navController.navigate(
+                    "detail/title/description}"
+                )
+            }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            Text(
+                "title",
+                fontSize = 16.sp,
+                fontStyle = FontStyle.Normal,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                "Description",
+                fontSize = 12.sp,
+                fontStyle = FontStyle.Normal,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+
 }
